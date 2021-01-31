8450bd50f65a01e592ed529aafebe2cabcc24cbf
Viorel <dociu_v@yahoo.com>
2021-01-31 09:54:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import './App.css'
-import Navbar from './components/navbar/navbar'
-import Errorpage from './pages/Errorpage'
-import PrivateRoute from './routes/PrivateRoute'
-import Dashboard from './pages/Dashboard'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import { auth } from './init-firebase'
-import { logInUser } from './store/actions/actions'
-import Create from './pages/Create'
+import React from 'react'
+import './app.css'
+import Dashboard from './components/Dashboard'
 
-const App = ({ uid, logInUser }) => {
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        // console.log('user from app', user)
-        logInUser(user)
-      } else {
-        // console.log('onAuthStateChanged you are log out')
-      }
-    })
-  }, [])
-
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path='/signin' component={Signin} />
-        <Route exact path='/signup' render={() => <Signup />} />
-        <PrivateRoute exact path='/' component={Dashboard} />
-        {/* <PrivateRoute exact path='/create' component={Create} /> */}
-        <PrivateRoute exact path='/create' component={Create} />
-        <Route exact path='/home' component={Home} />
-        <Route path='*' component={Errorpage} />
-      </Switch>
-    </BrowserRouter>
+    <main>
+      <Dashboard />
+    </main>
   )
 }
-const mapStateToProps = (state) => {
-  const { uid } = state.authState
-  return { uid }
-}
 
-export default connect(mapStateToProps, { logInUser })(App)
+export default App
--- src/store/actions/listTypes.js
+++
@@ -1,3 +0,0 @@
-export const FETCH_LISTS = 'FETCH_LISTS'
-export const FETCH_LISTS_FAIL = 'FETCH_LISTS_FAIL'
-export const SET_CURRENT_LIST = 'SET_CURRENT_LIST'
